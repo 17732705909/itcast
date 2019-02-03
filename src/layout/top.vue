1f7935b625125b37b3f957b36d45227a6b9c38c3
zhanglei <418538746@qq.com>
2019-02-03 13:34:23 +0800

    Create top.vue

---
+++ src/layout/top.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="6">
+                <div class="grid-content bg-purple h-logo">
+                    <img src="../assets/images/logo.png" alt="">
+                </div>
+            </el-col>
+            <el-col :span="12">
+                <div class="grid-content bg-purple-light h-title">
+                    <h1>电商后台管理系统</h1>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple h-logout">
+                    <a href="#" @click.prevent="logout()">退出</a>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout() {
+            this.$confirm('您将退出登录, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            })
+            .then(() => {
+                this.$message({
+                    type: 'success',
+                    message: '退出成功!'
+                });
+                window.localStorage.removeItem('token')
+                this.$router.push('/login')          
+            })
+            .catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消退出'
+                });
+            });
+        }
+    }
+}
+</script>
+
+<style>
+.h-title h1 {
+    color: #fff;
+    font-weight: 400;
+    text-align: center;
+}
+.h-logout  {
+    margin-right: 10px;
+    text-align: right;
+}
+.h-logout a {
+    color: #fff;
+    text-decoration: none;
+}
+</style>
